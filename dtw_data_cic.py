c4b403c2c38fbb5e5f01cf92dcef9262f0c76a37
Stefan Machmeier <stefan-machmeier@outlook.com>
2024-05-21 10:36:43 +0200

    Format files

--- dtw_data_cic.py
+++ dtw_data_cic.py
@@ -3,6 +3,7 @@ import numpy as np
 
 from itertools import pairwise
 
+
 def multidimensional_to_numpy(s):
     dimensions = [1, len(s)]
     while s.dtype == pl.List:
@@ -14,58 +15,88 @@ def multidimensional_to_numpy(s):
 
 if __name__ == "__main__":
     df_attacks = []
-    for file in ["heavy_image", "heavy_audio", "heavy_compressed", "heavy_exe", "heavy_text", "heavy_video"]:   
-        df_attack = pl.read_csv(f"/home/smachmeier/Downloads/DNSExif/Attacks/{file}.csv", has_header=False, separator="\t",try_parse_dates=True).with_columns(
+    for file in [
+        "heavy_image",
+        "heavy_audio",
+        "heavy_compressed",
+        "heavy_exe",
+        "heavy_text",
+        "heavy_video",
+    ]:
+        df_attack = pl.read_csv(
+            f"/home/smachmeier/Downloads/DNSExif/Attacks/{file}.csv",
+            has_header=False,
+            separator="\t",
+            try_parse_dates=True,
+        ).with_columns(
             [
-                (pl.col("column_1").str.to_datetime("%B %d, %Y %H:%M:%S%.f CET", time_unit="ms")),
-                (pl.lit("1")).alias("class")
+                (
+                    pl.col("column_1").str.to_datetime(
+                        "%B %d, %Y %H:%M:%S%.f CET", time_unit="ms"
+                    )
+                ),
+                (pl.lit("1")).alias("class"),
             ]
         )
-        df_attack: pl.DataFrame = df_attack.sort("column_1").group_by_dynamic("column_1", every="1m", closed="right", by=["column_2", "class"]).agg(pl.col("column_3").count())
+        df_attack: pl.DataFrame = (
+            df_attack.sort("column_1")
+            .group_by_dynamic(
+                "column_1", every="1m", closed="right", by=["column_2", "class"]
+            )
+            .agg(pl.col("column_3").count())
+        )
 
         df_attacks.append(df_attack[:100])
-    df_benigns=[]
-    for file in ["benign_heavy_1", "benign_heavy_2", "benign_heavy_3"]:  
-        df_benign = pl.read_csv(f"/home/smachmeier/Downloads/DNSExif/Benign/{file}.csv", has_header=False, separator="\t",try_parse_dates=True).with_columns(
+    df_benigns = []
+    for file in ["benign_heavy_1", "benign_heavy_2", "benign_heavy_3"]:
+        df_benign = pl.read_csv(
+            f"/home/smachmeier/Downloads/DNSExif/Benign/{file}.csv",
+            has_header=False,
+            separator="\t",
+            try_parse_dates=True,
+        ).with_columns(
             [
-                (pl.col("column_1").str.to_datetime("%B %d, %Y %H:%M:%S%.f CET", time_unit="ms")),
-                (pl.lit("0")).alias("class")
+                (
+                    pl.col("column_1").str.to_datetime(
+                        "%B %d, %Y %H:%M:%S%.f CET", time_unit="ms"
+                    )
+                ),
+                (pl.lit("0")).alias("class"),
             ]
         )
-        
-        df_benign: pl.DataFrame = df_benign.sort("column_1").group_by_dynamic("column_1", every="1m", closed="right", by=["column_2", "class"]).agg(pl.col("column_3").count())
+
+        df_benign: pl.DataFrame = (
+            df_benign.sort("column_1")
+            .group_by_dynamic(
+                "column_1", every="1m", closed="right", by=["column_2", "class"]
+            )
+            .agg(pl.col("column_3").count())
+        )
 
         df_benigns.append(df_benign[:100])
-    
-    total = df_benigns + df_attacks
-        
-    df = pl.concat(total)
-    
-    df = df.rename(
-        {
-            "column_1" : "timestamp",
-            "column_2" : "client",
-            "column_3" : "size" 
-        }
-    )
-    
-    x = df
-    
-    print(x)
 
-    x = x.with_columns([
-        pl.col(i).rank("dense").cast(pl.Int64).name.suffix("_encoded") for i in ["client", "class"]
-    ])
+    total = df_benigns + df_attacks
+
+    df = pl.concat(total)
+
+    df = df.rename({"column_1": "timestamp", "column_2": "client", "column_3": "size"})
+
+    x = df
+
+    x = x.with_columns(
+        [
+            pl.col(i).rank("dense").cast(pl.Int64).name.suffix("_encoded")
+            for i in ["client", "class"]
+        ]
+    )
     x = x.drop(["client", "class"])
 
     x = x.group_by(["client_encoded", "class_encoded"]).agg(pl.col("size"))
-    
-    print(x)
 
     Y = x.select(["class_encoded"])
     x = x.select(["size"])
 
     x = multidimensional_to_numpy(x["size"])
-    # np.save("x_cic_h_packet.npy", x)
+    np.save("data/x_cic_min_packet_count.npy", x)
     Y = Y.to_numpy()
-    # np.save("y_cic_h_packet.npy", Y)
+    np.save("data/y_cic_min_packet_count.npy", Y)
