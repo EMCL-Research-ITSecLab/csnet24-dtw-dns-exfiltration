fa351435e4816cc49fabed475c73712ba97b23d0
Stefan Machmeier <stefan-machmeier@outlook.com>
2024-06-17 15:47:11 +0200

    Update live sniffer

--- live_sniffer.py
+++ live_sniffer.py
@@ -1,6 +1,7 @@
 import codecs
 import datetime
 import sched
+import schedule
 from multiprocessing import Queue
 
 import joblib
@@ -29,15 +30,11 @@ class Sniffer(Thread):
 class Consumer(Thread):
     """Consumer"""
 
-    def __init__(self, que, model_path: str = "./dtwknn.pickle"):
+    def __init__(self, que_data: Queue, que_validator: Queue):
         super().__init__()
-        self.que = que
-        self.channels = []
+        self.que = que_data
+        self.que_validator = que_validator
         self.data = []
-        self.s = sched.scheduler(time.time, time.sleep)
-        self.model = joblib.load(model_path)
-    
-    
 
     def run(self):
         while True:
@@ -70,71 +67,82 @@ class Consumer(Thread):
                 p["packet_size"] = packet_size
                 p["query"] = query
 
-                self.channels.append(p)
+                self.que_validator.put(p)
 
-    def check(self):
-        if len(self.channels) > 1:
-            df_total = pl.DataFrame(self.channels)
-            df = (
-                df_total.sort("timestamp")
-                .group_by_dynamic(
-                    "timestamp", every="2s", closed="right", by=["src_ip", "dst_ip"]
+class Validator(Thread):
+    """Consumer"""
+
+    def __init__(self, que: Queue, model_path: str = "./dtwknn.pickle"):
+        super().__init__()
+        self.que: Queue = que
+        self.data = []
+        self.channels = []
+        self.model = joblib.load(model_path)
+
+    def run(self):
+        while True:
+            self.channels.append(self.que.get())
+            if len(self.channels) > 1:
+                df_total = pl.DataFrame(self.channels)
+                df = (
+                    df_total.sort("timestamp")
+                    .group_by_dynamic(
+                        "timestamp", every="2s", closed="right", by=["src_ip", "dst_ip"]
+                    )
+                    .agg(pl.col("entropy").mean(), pl.col("packet_size").mean())
                 )
-                .agg(pl.col("entropy").mean(), pl.col("packet_size").mean())
-            )
-            min_date = df.select(["timestamp"]).min().item()
-            max_date = df.select(["timestamp"]).max().item()
+                min_date = df.select(["timestamp"]).min().item()
+                max_date = df.select(["timestamp"]).max().item()
 
-            # We generate empty datetime with zero values in a time range of 6h
-            datetimes = df.select(
-                pl.datetime_range(
-                    min_date.replace(microsecond=0),
-                    max_date.replace(microsecond=0),
-                    "1s",
-                    time_unit="us",
-                ).alias("timestamp")
-            )
+                # We generate empty datetime with zero values in a time range of 6h
+                datetimes = df.select(
+                    pl.datetime_range(
+                        min_date.replace(microsecond=0),
+                        max_date.replace(microsecond=0),
+                        "1s",
+                        time_unit="us",
+                    ).alias("timestamp")
+                )
 
-            ids = df.select(["src_ip", "dst_ip"]).unique()
+                ids = df.select(["src_ip", "dst_ip"]).unique()
 
-            # Cross joining all domain
-            all_dates = datetimes.join(ids, how="cross")
-            # Fill with null
-            x = all_dates.join(df, how="left", on=["src_ip", "dst_ip", "timestamp"]).fill_null(0)
+                # Cross joining all domain
+                all_dates = datetimes.join(ids, how="cross")
+                # Fill with null
+                x = all_dates.join(df, how="left", on=["src_ip", "dst_ip", "timestamp"]).fill_null(0)
 
-            x_column = x.group_by(["src_ip", "dst_ip"]).agg(pl.col("entropy"))
+                x_column = x.group_by(["src_ip", "dst_ip"]).agg(pl.col("entropy"))
 
-            unique_ips = x_column.select(["src_ip", "dst_ip"]).unique()
-            for row in unique_ips.rows(named=True):
-                data = x_column.filter(pl.col("src_ip") == row["src_ip"])
-                
-                data_entropy = data.select("entropy").rows(named=True)[0]["entropy"]
-                
-                # Time interval 5 sec.
-                if len(data_entropy) > 4:
-                    result = self.model.predict(np.asarray(data_entropy[:5]))
-                    if result[0] == 2:
-                        print(f"Prediction for data: {result}")
-                        print(f"Suspicious Data: {df_total.filter(pl.col('src_ip') == row['src_ip']).sort('timestamp')}")
+                unique_ips = x_column.select(["src_ip", "dst_ip"]).unique()
+                for row in unique_ips.rows(named=True):
+                    data = x_column.filter(pl.col("src_ip") == row["src_ip"])
+                    
+                    data_entropy = data.select("entropy").rows(named=True)[0]["entropy"]
+                    
+                    # Time interval 5 sec.
+                    if len(data_entropy) > 4:
+                        result = self.model.predict(np.asarray(data_entropy[:5]))
+                        if result[0] == 2:
+                            print(f"Prediction for data: {result}")
+                            print(f"Suspicious Data: {df_total.filter(pl.col('src_ip') == row['src_ip']).sort('timestamp')}")
 
-                    # self.data.append(data)
-                    # y = np.full((len(self.data), 1), 1)
-                    # np.save(f"data/x_testg_1min_entropy.npy", self.data)
-                    # np.save(f"data/y_testg_1min_entropy.npy", y)
+                        # self.data.append(data)
+                        # y = np.full((len(self.data), 1), 1)
+                        # np.save(f"data/x_testg_1min_entropy.npy", self.data)
+                        # np.save(f"data/y_testg_1min_entropy.npy", y)
 
-                    # Clear channels
-                    self.channels = []
-        self.s.enter(1, 1, self.check)
-        self.s.run()
-
+                        # Clear channels
+                        self.channels = []
 
 def main():
-    que = Queue()
-    sniffer = Sniffer(que)
-    consumer = Consumer(que)
+    que_data = Queue()
+    que_validate = Queue()
+    sniffer = Sniffer(que_data)
+    consumer = Consumer(que_data, que_validate)
+    validator = Validator(que_validate)
     sniffer.start()
     consumer.start()
-    consumer.check()
+    validator.start()
 
 
 if __name__ == "__main__":
