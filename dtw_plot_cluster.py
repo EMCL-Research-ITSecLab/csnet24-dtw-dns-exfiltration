87ba66ca895a7ca5df1103294a6a386af4d4deae
Stefan Machmeier <stefan-machmeier@outlook.com>
2024-07-24 15:45:28 +0200

    Update clusters

--- dtw_plot_cluster.py
+++ dtw_plot_cluster.py
@@ -1,10 +1,7 @@
 import matplotlib.pyplot as plt
-import numpy as np
 from tslearn.barycenters import (
-    euclidean_barycenter,
     dtw_barycenter_averaging,
     dtw_barycenter_averaging_subgradient,
-    softdtw_barycenter,
 )
 
 from utils import TIME_INTERVAL_CONFIG, load_dataset
@@ -18,20 +15,20 @@ def plot_helper(barycenter):
     plt.plot(barycenter.ravel(), "r-", linewidth=2)
 
 
-def plot_dtw_path(X, y, type: str):
-    length_of_sequence = X.shape[1]
+def plot_dtw_path(x, y, type: str):
+    length_of_sequence = x.shape[1]
 
     # plot the four variants with the same number of iterations and a tolerance of
     # 1e-3 where applicable
-    ax1 = plt.subplot(4, 1, 1)
+    ax1 = plt.subplot(2, 1, 1)
 
-    plt.subplot(4, 1, 2, sharex=ax1)
+    # plt.subplot(2, 1, 1, sharex=ax1)
     plt.title("DBA (vectorized version of Petitjean's EM)")
-    plot_helper(dtw_barycenter_averaging(X, max_iter=50, tol=1e-3))
+    plot_helper(dtw_barycenter_averaging(x, max_iter=50, tol=1e-5))
 
-    plt.subplot(4, 1, 3, sharex=ax1)
+    plt.subplot(2, 1, 2, sharex=ax1)
     plt.title("DBA (subgradient descent approach)")
-    plot_helper(dtw_barycenter_averaging_subgradient(X, max_iter=50, tol=1e-3))
+    plot_helper(dtw_barycenter_averaging_subgradient(x, max_iter=50, tol=1e-5))
 
     # clip the axes for better readability
     ax1.set_xlim([0, length_of_sequence])
@@ -40,10 +37,12 @@ def plot_dtw_path(X, y, type: str):
     plt.tight_layout()
     plt.show()
     plt.savefig(f"figs/cluster_{type}.pdf")
+    ax1.cla()
+    plt.clf()
 
 if __name__ == "__main__":
     for ti in TIME_INTERVAL_CONFIG:
-        X, y = load_dataset(
+        X, y, _, _ = load_dataset(
             ti["time_interval_name"],
             data=[
                 {
@@ -53,5 +52,6 @@ if __name__ == "__main__":
                 }
             ],
         )
-
-        plot_dtw_path(X, y, ti["time_interval_name"])
+        print(X[0:1])
+        plot_dtw_path(X[0:1], y, ti["time_interval_name"])
+        plt.clf()
