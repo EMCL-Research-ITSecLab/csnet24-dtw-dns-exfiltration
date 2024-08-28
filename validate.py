caf8bb23a53ac348efb3ea8f8967b769b79dc48f
Stefan Machmeier <stefan-machmeier@outlook.com>
2024-08-28 14:22:36 +0200

    Add step wise analyse

--- validate.py
+++ validate.py
@@ -22,45 +22,42 @@ def validate(name):
             clf = joblib.load(
                 f"/mnt/data/models/model_{name}_{ti['time_interval_name']}_{ts}.pickle"
             )
-            result = dict()
+            
+            for i in len(HEICLOUD_DATA):
+                result = dict()
 
-            result["time_interval"] = ti["time_interval_name"]
-            result["ts_type"] = ts
-            result["model"] = name
+                result["time_interval"] = ti["time_interval_name"]
+                result["ts_type"] = ts
+                result["model"] = name
+                result["day"] = HEICLOUD_DATA[i]
 
-            print(f"Run analysis on data: {ti['time_interval_name']} for {ts}")
+                print("Predicting production set...")
+                result["prod"] = {}
+                
+                X_new, y_new, _, _ = load_dataset(
+                    ti["time_interval_name"], ts_type=ts, data=[HEICLOUD_DATA[i]]
+                )
+                
+                print(f"Data has shape: {X_new.shape}")
 
-            X, y, _, _ = load_dataset(ti["time_interval_name"], ts_type=ts)
+                y_pred = clf.predict(X_new)
+                report = classification_report(y_new, y_pred, output_dict=True)
+                fttar_test = fttar(y_new, y_pred)
+                fpr_test = fpr(y_new, y_pred)
+                fdr_test = fdr(y_new, y_pred)
 
-            print(f"Data has shape: {X.shape}")
+                result["prod"]["report"] = report
+                result["prod"]["fttar"] = fttar_test
+                result["prod"]["fpr"] = fpr_test
+                result["prod"]["fdr"] = fdr_test
 
+                print(report)
+                print(f"FTTAR: {fttar_test}")
+                print(f"False Positive Rate: {fpr_test}")
+                print(f"False Discovery Rate: {fdr_test}")
 
-
-            print("Predicting production set...")
-            result["prod"] = {}
-
-            X_new, y_new, _, _ = load_dataset(
-                ti["time_interval_name"], ts_type=ts, data=HEICLOUD_DATA
-            )
-
-            y_pred = clf.predict(X_new)
-            report = classification_report(y_new, y_pred, output_dict=True)
-            fttar_test = fttar(y_new, y_pred)
-            fpr_test = fpr(y_new, y_pred)
-            fdr_test = fdr(y_new, y_pred)
-
-            result["prod"]["report"] = report
-            result["prod"]["fttar"] = fttar_test
-            result["prod"]["fpr"] = fpr_test
-            result["prod"]["fdr"] = fdr_test
-
-            print(report)
-            print(f"FTTAR: {fttar_test}")
-            print(f"False Positive Rate: {fpr_test}")
-            print(f"False Discovery Rate: {fdr_test}")
-
-            with open(f"result_{ti['time_interval_name']}.json", "a+") as f:
-                f.write(json.dumps(result) + "\n")
+                with open(f"result_{ti['time_interval_name']}.json", "a+") as f:
+                    f.write(json.dumps(result) + "\n")
 
 if __name__ == "__main__":
     name = sys.argv[1]
